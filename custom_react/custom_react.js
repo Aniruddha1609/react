1be381b7990e946984642efe4f423e6b2264c72e
Aniruddha1609 <anniruddha2003@gmail.com>
2026-07-30 15:03:18 +0530

    feat: refactor customRender function to use dynamic attribute setting for props

--- custom_react/custom_react.js
+++ custom_react/custom_react.js
@@ -1,11 +1,21 @@
-function customRender(reactElement, mainContainer) {
+// function customRender(reactElement, mainContainer) {
+//     const domElement = document.createElement(reactElement.type);
+//     domElement.innerHTML = reactElement.children;
+//     domElement.setAttribute("href", reactElement.props.href);
+//     domElement.setAttribute("target", reactElement.props.target);
+//     mainContainer.appendChild(domElement);
+// }
+
+// better approach
+
+function customRender() {
     const domElement = document.createElement(reactElement.type);
     domElement.innerHTML = reactElement.children;
-    domElement.setAttribute("href", reactElement.props.href);
-    domElement.setAttribute("target", reactElement.props.target);
+    for (const prop in reactElement.props) {
+        domElement.setAttribute(prop, prop);
+    }
     mainContainer.appendChild(domElement);
 }
-
 const reactElement = {
     type: "a",
     props: {
