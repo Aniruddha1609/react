982ec210b6d74dd95e41c934af8367993d531130
Aniruddha1609 <anniruddha2003@gmail.com>
2026-08-04 17:51:43 +0530

    Add initial setup for React application with Vite
    - Added React and Vite configuration files.
    - Created main entry point for the application.
    - Included SVG assets for React and Vite logos.

---
+++ 04bg_changer/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+
+function App() {
+  const [bgColor, setBgColor] = useState("black");
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
