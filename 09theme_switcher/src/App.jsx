7f7ca3e27ded44b6fa80d773dca7a21a69e0b0ab
Aniruddha1609 <anniruddha2003@gmail.com>
2026-08-19 21:51:53 +0530

    feat: implement theme context with light and dark mode functionality

--- 09theme_switcher/src/App.jsx
+++ 09theme_switcher/src/App.jsx
@@ -1,10 +1,36 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { ThemeProvider } from "./contexts/theme";
 
 function App() {
+  const [themeMode, setThemeMode] = useState("light");
+
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
+
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+
+  useEffect(() => {
+    const html = document.querySelector("html");
+    html.classList.remove("light", "dark");
+    html.classList.add(themeMode);
+  }, [themeMode]);
+
   return (
-    <>
-      <h1 bg-amber-700>Theme Switcher</h1>
-    </>
+    <ThemeProvider value={{ themeMode, lightTheme, darkTheme }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
