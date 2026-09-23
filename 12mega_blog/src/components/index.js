888eb1b01ce1cd68e76624537bfa08ee85f16fcf
Aniruddha1609 <anniruddha2003@gmail.com>
2026-09-23 16:14:26 +0530

    feat: add authentication components and enhance component exports

--- 12mega_blog/src/components/index.js
+++ 12mega_blog/src/components/index.js
@@ -3,5 +3,19 @@ import Footer from "./footer/footer";
 import Header from "./header/Header";
 import LogoutBtn from "./header/LogoutBtn";
 import Container from "./container/container";
+import Button from "./Button";
+import Select from "./Select";
+import Postcard from "./Postcard";
+import Input from "./Input";
 
-export { Container, Footer, Header, Logo, LogoutBtn };
+export {
+    Button,
+    Container,
+    Footer,
+    Header,
+    Input,
+    Logo,
+    LogoutBtn,
+    Postcard,
+    Select,
+};
